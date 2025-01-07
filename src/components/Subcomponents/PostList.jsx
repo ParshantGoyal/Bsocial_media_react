0bb960d131cc97e556bd856534c23bae0f45d485
ParshantGoyal <147721555+ParshantGoyal@users.noreply.github.com>
2025-01-07 11:47:06 +0000

    context setup

--- src/components/Subcomponents/PostList.jsx
+++ src/components/Subcomponents/PostList.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
-import CPost from "./Post";
+import Cpost from "./SinglePost";
 import{PostList as Postlistdata} from "../../store/post-list-store";
 
 const PostList =()=>{
-  const {PostList} = useContext(Postlistdata);
+  const {postListm} = useContext(Postlistdata);
+ 
 return(<>
-{/* {PostList.map((item)=>{<CPost key={item.id} postc={item}/>})} */}
-<CPost/>
+
+{postListm.map((post) => <Cpost key={post.id} item={post}/>)}
+
+
 
 </>)
 
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,39 +1,72 @@
-import { type } from "@testing-library/user-event/dist/types/utility";
+
 import { createContext, useReducer } from "react";
 
 
-export const PostList =createContext({postList:[],
+export const PostList =createContext({postListm:[],
     addPost:()=>{},
     deletePost:()=>{},
 });
 
 const postListReducer =(currPostList,action)=>{
    let newPostList = currPostList;
-    if(action.type ==='DELETE_POST'){
-  newPostList.filter((item) => item.id !== action.payload.postId)
+    if(action.type ==="DELETE_POST"){
+   return newPostList.filter((ite) => ite.id !== action.payload.ID)  
     }
-    
-    return currPostList;
+    else if(action.type==='CREATE_POST'){
+    newPostList= [action.payload,...currPostList]  
+ }   ;  
+    return newPostList
 }
+
+
 
 const PostListProvider=({children})=>{
+const[postListm,dispatchPostList]=useReducer(postListReducer,Default_v);
 
-const[postList,dispatchPostList]=useReducer(postListReducer,[])
 
-const addPost=()=>{
-
+const addPost=(userId,postTitle,postBody,Reactions,Tags)=>{
+// console.log(postListm);
+    dispatchPostList({
+        type:'CREATE_POST',
+        payload:{
+            id:userId,
+            title:postTitle,
+            body:postBody,
+            reaction:Reactions,
+            tag:Tags,
+            
+        }
+    })
 }
-const deletePost=( postId)=>{
-dispatchPostList({
-    type:'DELETE_POST',
+const deletePost=(postId)=>{
+    console.log(postId)
+    dispatchPostList({
+    type:"DELETE_POST",
     payload:{
-        postId
-    }
-})
+        ID:postId,
+    },
+});
+};
+
+return (<PostList.Provider value={{postListm,addPost,deletePost}}>{children}</PostList.Provider>)
+
 }
 
-return <PostList.Provider value={{ postList,addPost,deletePost,}
-}>{children}</PostList.Provider>
-
+const Default_v =[
+    {
+    id:1,
+    title:"postTitle",
+    body:"postBody",
+    reaction:80,
+    tag:['Tags','Tags2'],
+},
+{
+    id:2,
+    title:"postTitlebhjh",
+    body:"postBody",
+    reaction:80,
+    tag:['Tags','mm'],
 }
+]
+
 export default PostListProvider;
